95f24002619785d778f035910c2bb17e000094fe
Abhi-siddheshware <siddheshwareabhay@gmail.com>
2024-12-26 19:57:13 +0530

    first question simple hello program

---
+++ project1_hello/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Runtime.InteropServices;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace project1_hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#");
+           
+            int i = 100;
+            Console.WriteLine(i);
+            
+
+
+            //while (true)
+            //{
+            //    Console.WriteLine("Enter your first and second number:");
+            //    int n1 = int.Parse(Console.ReadLine());
+            //    int n2 = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("enter the operation");
+            //    Console.WriteLine("1.ADD \n 2.SUB \n 3.MUL \n 4.DIV \n 5.MOD");
+            //    int choice = int.Parse(Console.ReadLine());
+            //    switch (choice)
+            //    {
+            //        case 1:
+            //            int add = n1 + n2;
+            //            Console.WriteLine(add);
+            //            break;
+            //        case 2:
+            //            int sub = n1 - n2;
+            //            Console.WriteLine(sub);
+            //            break;
+            //        case 3:
+            //            int mul = n1 * n2;
+            //            Console.WriteLine(mul);
+            //            break;
+            //        case 4:
+            //            int div = n1 / n2;
+            //            Console.WriteLine(div);
+            //            break;
+            //        case 5:
+            //            int mod = n1 % n2;
+            //            Console.WriteLine(mod);
+            //            break;
+
+
+            //        default:
+            //            Console.WriteLine("enter valid operation !!!");
+            //            break;
+
+            //            Console.ReadLine();
+            //    }
+            Console.ReadLine();
+            
+        }
+        
+    }
+    
+}
