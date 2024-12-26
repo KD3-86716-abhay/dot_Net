ba94046b9207e053aeee56d2f6c3f723540ab082
Abhi-siddheshware <siddheshwareabhay@gmail.com>
2024-12-26 20:02:38 +0530

    second question updated

---
+++ Question2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    internal class Program
+    {   
+        static void Main(string[] args)
+        {
+           int x=int.Parse(args[0]);
+            int y=int.Parse(args[1]);
+            string op=args[2];
+
+
+            int res = 0;
+            if (op == "+")
+            {
+                res = x + y;
+            }
+            else if (op == "-")
+            {
+                res = x - y;
+            }
+            else if (op == "*")
+            {
+                res = x * y;
+            }
+            else
+            {
+                res = x / y;
+            }
+            Console.WriteLine(res);
+        }
+
+
+        
+    }
+}
